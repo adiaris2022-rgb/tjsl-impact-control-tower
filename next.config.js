16f8dffc2e005c4e390752c33f35283151f2d1fc
Adi Aris <adi.aris.2022@gmail.com>
2026-09-19 09:11:46 +0700

    fix: disable html caching on control tower

--- next.config.js
+++ next.config.js
@@ -2,5 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+  generateEtags: false,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "Cache-Control", value: "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0" },
+          { key: "Pragma", value: "no-cache" },
+          { key: "Expires", value: "0" },
+        ],
+      },
+    ];
+  },
 };
 module.exports = nextConfig;
